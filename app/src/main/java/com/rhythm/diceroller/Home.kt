f4eab9d2d5b727ff0f14314d1e2b4cbf6e1bd55e
Rhythm <rajgururhythm493@gmail.com>
2024-08-07 15:05:33 +0530

    random no. implemented and images added

--- app/src/main/java/com/rhythm/diceroller/Home.kt
+++ app/src/main/java/com/rhythm/diceroller/Home.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +15,14 @@ class Home : AppCompatActivity() {
         val rollButton:Button=findViewById(R.id.roll_btn)
 
         rollButton.setOnClickListener {
-            rollDice()
+            rollDice()//function called
         }
     }
 
     private fun rollDice() {
         val resultText:TextView=findViewById(R.id.result_text)
-        resultText.text="Dice Rolled!"
+        val randomInt=java.util.Random().nextInt(6)+1
+        //it will generate 0 to 5 numbers but we need 0 to 6 that's why we added +1
+        resultText.text=randomInt.toString()//int converted into String
     }
 }
