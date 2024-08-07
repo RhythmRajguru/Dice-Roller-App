3bfda8c7814fdf19b26c1721a9f47cc57d6c5f8b
Rhythm <rajgururhythm493@gmail.com>
2024-08-07 15:25:13 +0530

    image setted and logic implemented

--- app/src/main/java/com/rhythm/diceroller/Home.kt
+++ app/src/main/java/com/rhythm/diceroller/Home.kt
@@ -2,27 +2,43 @@ package com.rhythm.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class Home : AppCompatActivity() {
+    lateinit var diceImage:ImageView
+    lateinit var rollButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val rollButton:Button=findViewById(R.id.roll_btn)
+        diceImage=findViewById(R.id.dice_image)
+        rollButton=findViewById(R.id.roll_btn)
 
         rollButton.setOnClickListener {
+
             rollDice()//function called
         }
     }
+    fun rollDice() {
 
-    private fun rollDice() {
-        val resultText:TextView=findViewById(R.id.result_text)
-        val randomInt=java.util.Random().nextInt(6)+1
+
         //it will generate 0 to 5 numbers but we need 0 to 6 that's why we added +1
-        resultText.text=randomInt.toString()//int converted into String
+        val drawableResourse=when(java.util.Random().nextInt(6)+1)
+        {
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResourse)
+
+
     }
 }
